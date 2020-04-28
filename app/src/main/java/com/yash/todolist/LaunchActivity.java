da6464dee45a19ec1f153de64b483a2e71a7904f
Yash Sompura <sompurayash@live.com>
2020-04-28 12:56:35 -0400

    Corrected Splash screen as per Google guidelines

--- app/src/main/java/com/yash/todolist/LaunchActivity.java
+++ app/src/main/java/com/yash/todolist/LaunchActivity.java
@@ -11,32 +11,11 @@ import android.widget.ProgressBar;
 
 public class LaunchActivity extends AppCompatActivity {
 
-    private static final int TIME = 2000;
-    private ProgressBar progressBar;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_launch);
-
-        progressBar = findViewById(R.id.splashProgress);
-        startAnimation();
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(LaunchActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        }, TIME);
-    }
-
-    public void startAnimation()    {
-        ObjectAnimator.ofInt(progressBar, "Progress", 100)
-                .setDuration(TIME)
-                .start();
+        Intent intent = new Intent(LaunchActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
